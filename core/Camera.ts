0a5b758d3dd21334638388a28079f265a534a4a0
Insopitus <jessefor@hotmail.com>
2020-11-23 00:59:32 +0800

    add matrix methods

--- core/Camera.ts
+++ core/Camera.ts
@@ -1,16 +1,17 @@
 // perspective camera only for the moment
 import { Matrix4 } from '../math/Matrix4'
 export class Camera {
-  modelMatrix:Matrix4  = new Matrix4()
+  modelMatrix: Matrix4 = new Matrix4()
   viewMatrix: Matrix4 = new Matrix4()// inverse of the modelMatrix
   projectionMatrix: Matrix4 = new Matrix4()
   constructor(public fov: number, public aspect: number, public near: number, public far: number) {
     const fovFactor = 1 / Math.tan(fov / 2)
-    this.projectionMatrix.set([
+    this.projectionMatrix.set(
       fovFactor / aspect, 0, 0, 0,
       0, fovFactor, 0, 0,
-      0, 0, -(far + near) / (far - near), -1,
-      0, 0, -2 * far * near / (far - near), 0
-    ])
+      0, 0, -(far + near) / (far - near), -2 * far * near / (far - near),
+      0, 0, -1, 0
+    )
   }
+  
 }
--- geometries/BoxGeometry.ts
+++ geometries/BoxGeometry.ts
@@ -1,27 +1,27 @@
-export class BoxGeometry{
-  vertices:Float32Array
-  indices:Float32Array
-  normals:Float32Array
-  uv:Float32Array
-  constructor(public width:number,public height:number,public depth:number){
+export class BoxGeometry {
+  vertices: Float32Array
+  indices: Float32Array
+  normals: Float32Array
+  uv: Float32Array
+  constructor(public width: number, public height: number, public depth: number) {
     const w = width
     const d = depth
     const h = height
     this.vertices = new Float32Array([
-      w/2,-d/2,h/2, -w/2,-d/2,h/2, -w/2,-d/2,h/2, -w/2,d/2,h/2, w/2,d/2,h/2, w/2,-d/2,h/2,   // top side
-      -w/2,d/2,-h/2, w/2,-d/2,-h/2, w/2,-d/2,-h/2, w/2,d/2,-h/2, -w/2,d/2,-h/2, w/2,-d/2,-h/2, // bottom side
-      -w/2,d/2,h/2, -w/2,d/2,-h/2, w/2,d/2,h/2, -w/2,d/2,-h/2, -w/2,d/2,-h/2, w/2,d/2,h/2, //front side
-      w/2,-d/2,h/2, -w/2,-d/2,h/2, w/2,-d/2,h/2, w/2,-d/2,h/2, w/2,-d/2,-h/2, w/2,-d/2,h/2, // back
-      -w/2,d/2,h/2, -w/2,-d/2,h/2, w/2,-d/2,h/2, -w/2,d/2,-h/2, -w/2,d/2,h/2, w/2,-d/2,h/2, // left
-      w/2,-d/2,h/2, w/2,d/2,h/2, -w/2,d/2,-h/2, w/2,-d/2,h/2, -w/2,d/2,-h/2, w/2,-d/2,-h/2, //right
+      w / 2, -d / 2, h / 2, -w / 2, -d / 2, h / 2, -w / 2, -d / 2, h / 2, -w / 2, d / 2, h / 2, w / 2, d / 2, h / 2, w / 2, -d / 2, h / 2,   // top side
+      -w / 2, d / 2, -h / 2, w / 2, -d / 2, -h / 2, w / 2, -d / 2, -h / 2, w / 2, d / 2, -h / 2, -w / 2, d / 2, -h / 2, w / 2, -d / 2, -h / 2, // bottom side
+      -w / 2, d / 2, h / 2, -w / 2, d / 2, -h / 2, w / 2, d / 2, h / 2, -w / 2, d / 2, -h / 2, -w / 2, d / 2, -h / 2, w / 2, d / 2, h / 2, //front side
+      w / 2, -d / 2, h / 2, -w / 2, -d / 2, h / 2, w / 2, -d / 2, h / 2, w / 2, -d / 2, h / 2, w / 2, -d / 2, -h / 2, w / 2, -d / 2, h / 2, // back
+      -w / 2, d / 2, h / 2, -w / 2, -d / 2, h / 2, w / 2, -d / 2, h / 2, -w / 2, d / 2, -h / 2, -w / 2, d / 2, h / 2, w / 2, -d / 2, h / 2, // left
+      w / 2, -d / 2, h / 2, w / 2, d / 2, h / 2, -w / 2, d / 2, -h / 2, w / 2, -d / 2, h / 2, -w / 2, d / 2, -h / 2, w / 2, -d / 2, -h / 2, //right
     ])
     this.normals = new Float32Array([
-      0,1,0, 0,1,0, 0,1,0, 0,1,0, 0,1,0, 0,1,0, 
-      0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0, 
-      0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 
-      0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1, 
-      -1,0,0, -1,0,0, -1,0,0, -1,0,0, -1,0,0, -1,0,0, 
-      1,0,0, 1,0,0, 1,0,0, 1,0,0, 1,0,0, 1,0,0, 
+      0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,
+      0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,
+      0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+      0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1,
+      -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,
     ])
   }
 }
